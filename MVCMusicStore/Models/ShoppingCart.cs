331b8e9214e34e500da5aed771815e9a5b3d26fd
Alejandro Pietry <pietrytrindade12@gmail.com>
2021-02-09 21:30:09 -0300

    Finalizando classe para manipular o carrinho

--- MVCMusicStore/Models/ShoppingCart.cs
+++ MVCMusicStore/Models/ShoppingCart.cs
@@ -3,33 +3,35 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Http;
-using System.Threading.Tasks;
-using System.Web;
 
 namespace MVCMusicStore.Models
 {
     /*O modelo ShoppingCart manipula o acesso a dados para a tabela de carrinhos.
      * Além disso, ele tratará a lógica de negócios para a adição e remoção de itens do carrinho de compras.
-     * 
-     * Como não queremos exigir que os usuários se inscrevam para uma conta apenas para adicionar itens ao carrinho de compras, 
-     * atribuíremos aos usuários um identificador exclusivo temporário (usando um GUID ou um identificador global exclusivo) 
+     *
+     * Como não queremos exigir que os usuários se inscrevam para uma conta apenas para adicionar itens ao carrinho de compras,
+     * atribuíremos aos usuários um identificador exclusivo temporário (usando um GUID ou um identificador global exclusivo)
      * quando acessarem o carrinho de compras. Armazenaremos essa ID usando a classe de sessão ASP.NET.
      */
-    
+
     public partial class ShoppingCart
     {
         private MusicStoreEntities _contextDB;
         public string ShoppingCartId { get; set; }
         public const string CartSessionKey = "CartId";
-        public ShoppingCart() { }
+
+        public ShoppingCart()
+        {
+        }
+
         public ShoppingCart(MusicStoreEntities context)
         {
             _contextDB = context;
         }
+
         /// <summary>
-        /// é um método estático que permite que nossos controladores obtenham um objeto de carrinho. 
-        /// Ele usa o método Getcarrinhoid para lidar com a leitura de cartid a partir da sessão do usuário. 
+        /// é um método estático que permite que nossos controladores obtenham um objeto de carrinho.
+        /// Ele usa o método Getcarrinhoid para lidar com a leitura de cartid a partir da sessão do usuário.
         /// O método getcarrinhoid requer o HttpContextBase para que ele possa ler o Carrinhoid do usuário a partir da sessão do usuário.
         /// </summary>
         /// <param name="context"></param>
@@ -71,12 +73,12 @@ namespace MVCMusicStore.Models
         {
             //get the matching cart and album instances
             var cartItem = _contextDB.Tab_Cart.SingleOrDefault(
-                c => c.CartId == ShoppingCartId 
+                c => c.CartId == ShoppingCartId
                 && c.AlbumId == album.AlbumId);
 
-            if(cartItem is null)
+            if (cartItem is null)
             {
-                //create a new cart item if no cart item exist 
+                //create a new cart item if no cart item exist
                 cartItem = new Cart
                 {
                     AlbumId = album.AlbumId,
@@ -95,7 +97,12 @@ namespace MVCMusicStore.Models
             _contextDB.SaveChangesAsync();
         }
 
-        public int RemoveCart(int id)
+        /// <summary>
+        ///  usa uma ID de álbum e a remove do carrinho do usuário. Se o usuário tiver apenas uma cópia do álbum em seu carrinho, a linha será removida.
+        /// </summary>
+        /// <param name="id">Id do album</param>
+        /// <returns></returns>
+        public int RemoveFromCart(int id)
         {
             //Get the cart
             var cartItem = _contextDB.Tab_Cart.Single(cart => cart.CartId == ShoppingCartId && cart.RecordId == id);
@@ -104,7 +111,7 @@ namespace MVCMusicStore.Models
 
             if (cartItem is not null)
             {
-                if(cartItem.Count > 1)
+                if (cartItem.Count > 1)
                 {
                     cartItem.Count--;
                     itemCount = cartItem.Count;
@@ -119,5 +126,83 @@ namespace MVCMusicStore.Models
             return itemCount;
         }
 
+        /// <summary>
+        /// remove todos os itens do carrinho de compras de um usuário.
+        /// </summary>
+        public void EmptyCart()
+        {
+            var cartItems = _contextDB.Tab_Cart.Where(cart => cart.CartId == ShoppingCartId);
+
+            foreach (var cartItem in cartItems)
+            {
+                _contextDB.Tab_Cart.Remove(cartItem);
+            }
+            //Save changes
+            _contextDB.SaveChangesAsync();
+        }
+
+        /// <summary>
+        /// recupera uma lista de CartItems para exibição ou processamento.
+        /// </summary>
+        public List<Cart> GetCartItems()
+        {
+            return _contextDB.Tab_Cart.Where(cart => cart.CartId == ShoppingCartId).ToList();
+        }
+
+        /// <summary>
+        /// recupera um número total de álbuns que um usuário tem em seu carrinho de compras.
+        /// </summary>
+        /// <returns></returns>
+        public int GetCount()
+        {
+            //Pega o itens no carrinho e soma eles
+            int? count = (from cartItems in _contextDB.Tab_Cart
+                          where cartItems.CartId == ShoppingCartId
+                          select (int?)cartItems.Count).Sum();
+            //return 0 se count is null
+            return count ?? 0;
+        }
+
+        public decimal GetTotal()
+        {
+
+            // Multiplique o preço do álbum pela contagem desse álbum para obter
+            // o preço atual de cada um desses álbuns no carrinho
+            // some todos os totais de preço do álbum para obter o total do carrinho
+            decimal? total = (from cartItems in _contextDB.Tab_Cart
+                              where cartItems.CartId == ShoppingCartId
+                              select (int?)cartItems.Count *
+                              cartItems.Album.Price).Sum();
+
+            return total ?? decimal.Zero;
+        }
+
+        public int CreateOrder(Order order)
+        {
+            decimal orderTotal = 0;
+
+            var cartItems = GetCartItems();
+
+            foreach (var item in cartItems)
+            {
+                var orderDetails = new OrderDetail
+                {
+                    AlbumId = item.AlbumId,
+                    OrderId = order.OrderId,
+                    UnitPrice = item.Album.Price,
+                    Quantity = item.Count
+                };
+                //set the order total of the shopping cart
+                orderTotal += (item.Count * item.Album.Price);
+                _contextDB.Tab_OrderDetail.Add(orderDetails);
+            }
+            order.Total = orderTotal;
+
+            _contextDB.SaveChangesAsync();
+            EmptyCart();
+
+            //retorna o ORDERID como numero de confirmacao
+            return order.OrderId;
+        }
     }
 }
